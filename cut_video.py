66401b15bff3a7e6a01d8eb2d7e308b8bd04d302
nataliawcislo <wcislonatalia1998@gmail.com>
2021-09-14 15:37:08 +0200

    improved file loading

--- cut_video.py
+++ cut_video.py
@@ -2,27 +2,25 @@ import cv2
 
 
 def cut_frame(number_frame_entry, path_file_entry):
-    name_video_file = str(path_file_entry.get())
-    cap = cv2.VideoCapture(name_video_file)
+    cap = cv2.VideoCapture(path_file_entry)
     success, image = cap.read()
     fps = cap.get(cv2.CAP_PROP_FPS)  # number of frames per second
     frame_count = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))  # number of frames in the movie
-    number_frame = int(number_frame_entry.get()) # how many frames do i want to extract from the video
-    frames_to_skip = (frame_count - 1) / (number_frame - 1)  # co ile ramek wyciagam kolejna
+    number_frame = int(number_frame_entry.get())  # how many frames do i want to extract from the video
+    frames_to_skip = (frame_count - 1) / (number_frame - 1)  # count frame
     count = 0  # the number of the currently processed frame
     frames_taken = 0  # how many frames have already been drawn
-    print("Z filmu o długości " + str(frame_count) + " ramek wyciągam " + str(input) + " ramek")
+    print("From a movie of length " + str(frame_count) + " number of frame " + str(input) + " frame")
     image_list = []
     while success:
         if (count == round(frames_taken * frames_to_skip)):
-            print("Wyciągam " + str(frames_taken + 1) + ". ramkę, w wideo ma numer " + str(count) + " na " + str(
+            print("TO take " + str(frames_taken + 1) + ". Frame, in video has number " + str(count) + " on " + str(
                 frame_count - 1))
             milisecond = round(count * 1000 / fps)
-            print("Ramka pochodzi z " + str(milisecond) + " milisekundy nagrania")
+            print("Frame from " + str(milisecond) + " milliseconds of recording")
+            image_list.append(image)
             frames_taken += 1
-            #cv2.imwrite(os.path.join(path,"frame%d.jpg" % count), image)
         success, image = cap.read()
-        image_list.append(image)
         count += 1
     cap.release()
     return image_list
--- main.py
+++ main.py
@@ -2,123 +2,93 @@ import os
 import cv2
 import tkinter as tk
 from tkinter import filedialog as fd
+from tkinter import messagebox as mb
 from cut_video import cut_frame
 
 
-#windows for dialog file
-def save_file(imageList):
+# windows for dialog file
+def save_files(imageList):
     wybrany_folder = fd.askdirectory()
     if wybrany_folder:
         number_frame = 0
         for image in imageList:
-            cv2.imwrite(os.path.join(wybrany_folder, "frame-namei%d.jpg" %  number_frame), image)
+            cv2.imwrite(os.path.join(wybrany_folder, "frame-%d.jpg" % (number_frame + 1)), image)
             number_frame += 1
 
+
+selected_file = ""
+
+
 def select_file():
-    filetypes = (
-        ("video file", '*.mp4')
+    filetypes = [("Video file", '*.mp4')]
+    global selected_file
+    selected_file = fd.askopenfilename(
+        title="Open a file",
+        initialdir='/',
+        filetypes=filetypes,
     )
-
-    filename = fd.askopenfilename(
-        title ="Open a file",
-        initiadir = '/',
-        filetypes = filetypes,
-    )
-    return True
+    if (selected_file != ""):
+        file_name = selected_file[(selected_file.rfind('/') + 1):]
+        select_file_label.config(text=file_name)
+    else:
+        select_file_label.config(text='File not chosen')
 
 
-
-#check enter to str
+# check enter to str
 def printmessage(check_entry_numer, check_entry_path):
     if check_entry_numer == True and check_entry_path == True:
-        done_label.config(text = "Done!")
-    if check_entry_numer == False and check_entry_path == True:
-        done_label.config(text = "Please enter the number again")
-    if check_entry_numer == True and check_entry_path == False:
-        done_label.config(text = "please enter the path file again")
+        done_label.config(text="Processing...")
+    elif check_entry_numer == False and check_entry_path == True:
+        done_label.config(text="Please enter a correct number")
+    elif check_entry_numer == True and check_entry_path == False:
+        done_label.config(text="Please choose a file")
     else:
-        done_label.config(text = "please enter the values again")
+        done_label.config(text="Please choose a file and enter a correct number")
+    pass
 
 
-#check enter  to number
+# check enter to number
 def check_entry_numer(input_entry):
-    if input_entry:
-        try:
-            if int(input_entry) >= 2:
-                return True
-        except ValueError:
-            return False
-    else:
+    try:
+        return int(input_entry) >= 2
+    except ValueError:
         return False
 
-#check enter to str
-# def check_entry_path(input_entry):
-#     if input_entry:
-#         try:
-#             str(input_entry)
-#             return True
-#         except:
-#             return False
-#     else:
-#         return False
 
 def start():
-    check_number = check_entry_numer(number_frame_entry.get())
-    #check_path = check_entry_path(path_file_entry.get())
-    move = select_file()
-    printmessage(check_number, move)
-    imageList = cut_frame(number_frame_entry, move)
-    save_file(imageList.count(), imageList)
+    is_number_correct = check_entry_numer(number_frame_entry.get())
+    is_video_chosen = selected_file != "" and selected_file != None
+    printmessage(is_number_correct, is_video_chosen)
+    if (is_number_correct and is_video_chosen):
+        imageList = cut_frame(number_frame_entry, selected_file)
+        mb.showinfo(title="Message", message="Choose file saving location")
+        save_files(imageList)
+        done_label.config(text="Done!")
+
 
 root = tk.Tk()
-root.geometry("450x200")
+root.geometry("390x180")
 root.title("Cut video")
-
-# declaring string variable
-# for storing name and password
-count_frame = tk.StringVar()
-path_file = tk.StringVar()
-massage_text = tk.StringVar()
-
-# defining a function that will
-# get the name and password and
-# print them on the screen
-
-def submit():
-    c_frame = count_frame.get()
-    p_file = path_file.get()
-
-    c_frame.set("")
-    p_file.set("")
+root.resizable(False, False)
 
 number_frame_label = tk.Label(root, text='Number of frames', font=('helvetica', 14, 'normal'))
 
-number_frame_entry = tk.Entry(root, textvariable=count_frame, font=('helvetica', 14, 'normal'))
+number_frame_entry = tk.Entry(root, textvariable=tk.StringVar(), font=('helvetica', 14, 'normal'))
 
-path_file_label = tk.Label(root, text='File path', font=('helvetica', 14, 'normal'))
+select_file_label = tk.Label(root, text='File not chosen', font=('helvetica', 12, 'normal'))
 
-path_file_button = tk.Button(root, text='Select file', highlightbackground = "Blue", highlightthickness = 2, command=select_file )
+select_file_button = tk.Button(root, text='Choose file', highlightbackground="blue", highlightthickness=2,
+                               command=select_file)
 
-start_button = tk.Button(root, text='Start', highlightbackground = "Blue", highlightthickness = 2, command=start )
+start_button = tk.Button(root, text='Start', highlightbackground="blue", highlightthickness=2, command=start)
 
 done_label = tk.Label(root, text="", font=('helvetica', 14, 'normal'))
 
-done_label.pack()
-
-number_frame_label.pack()
-number_frame_entry.pack()
-path_file_label.pack()
-path_file_entry.pack()
-start_button.pack()
-done_label.pack()
-
-number_frame_label.grid(row=0, column=0, sticky = 'W', padx=10,pady=5)
-number_frame_entry.grid(row=0, column=1, padx=30)
-path_file_label.grid(row=1, column=0, sticky = 'W',  padx=10, pady=5)
-path_file_entry.grid(row=1, column=1)
-start_button.grid(row=2, column=1)
-done_label.grid(row=3, column=1, columnspan=2, padx=10, pady=20)
+number_frame_label.grid(row=0, column=0, sticky='W', padx=20, pady=10)
+number_frame_entry.grid(row=0, column=1, padx=10)
+select_file_button.grid(row=1, column=0, padx=20, pady=5)
+select_file_label.grid(row=1, column=1, sticky='W', padx=10, pady=5)
+start_button.grid(row=2, column=1, padx=10, pady=5)
+done_label.grid(row=3, column=0, columnspan=2, padx =20, pady =20)
 
 root.mainloop()
-
-
